d0ba3cde3f8e31740fc2e7c19fd5b9d827f64c64
Rishi <gupta.rishi1@northeastern.edu>
2022-10-06 02:52:14 -0400

    adding files

---
+++ test_flaskr.py
@@ -0,0 +1,25 @@
+import os
+import tempfile
+
+import pytest
+import  GET_APIcopycopy as flaskr
+
+
+@pytest.fixture
+def client():
+    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+    flaskr.app.config['TESTING'] = True
+
+    with flaskr.app.test_client() as client:
+        with flaskr.app.app_context():
+            flaskr.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(flaskr.app.config['DATABASE'])
+
+
+def test_empty_db(client):
+
+    rv = client.get('/healthz')
+    assert b'Hello 200 ok' in rv.data 
